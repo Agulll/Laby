ab6a8e136f0cd9db0c9402261e0fc4dab44abf4c
Agulll <61034995+Agulll@users.noreply.github.com>
2020-03-12 19:51:08 +0100

    Update test_maze.cpp

--- test_maze.cpp
+++ test_maze.cpp
@@ -83,7 +83,7 @@
 bool anyof (std::array<std::array<cell, 5>,5> grid){
   for (std::size_t i = 0; i < grid.size(); i++) {
     for (std::size_t j = 0; j < grid[i].size(); j++) {
-      if (grid[i][j].visited){
+      if (grid[i][j].visited == true){
         return true;
       }
     }
@@ -108,7 +108,7 @@ bool anyof (std::array<std::array<cell, 5>,5> grid){
     std::cout << "starting.\n";
     //while (!std::any_of(grid.begin(), grid.end(), [](const auto&line){return std::any_of(line.begin(),line.end(),[](const auto&cell) {return cell.visited;});}))
     //while (anyof(grid)){
-    for (int i = 0; i < 101; i++)
+    for (int i = 0; i < 10; i++)
     {
     grid[b1.x][b1.y].visited = true;
     show(grid, b1);
@@ -132,31 +132,33 @@ bool anyof (std::array<std::array<cell, 5>,5> grid){
       b1.x = (b1.history.back().x);
       b1.y = (b1.history.back().y);
     }
-    else {
-      debug = "Foward_traking";
-      b1.history.push_back(grid[b1.x][b1.y]);
-      std::srand(time(0));
-      int r = rand() % Neighbors.size();
-      if (b1.y - Neighbors[r].y ==  1 && b1.x - Neighbors[r].x == 0) {
-        grid[b1.x][b1.y].t = false;
-        grid[b1.x][b1.y - 1].b = false;
-      }
-      if (b1.x - Neighbors[r].x == -1 && b1.y - Neighbors[r].y == 0) {
-        grid[b1.x][b1.y].r = false;
-        grid[b1.x + 1][b1.y].l = false;
-      }
-      if (b1.y - Neighbors[r].y == -1 && b1.x - Neighbors[r].x == 0) {
-        grid[b1.x][b1.y].b = false;
-        grid[b1.x][b1.y + 1].t = false;
-      }
-      if (b1.x - Neighbors[r].x ==  1 && b1.y - Neighbors[r].y == 0) {
-        grid[b1.x][b1.y].l = false;
-        grid[b1.x - 1][b1.y].r = false;
-      }
+    // else {
+    //   debug = "Foward_traking";
+    //   b1.history.push_back(grid[b1.x][b1.y]);
+    //   std::srand(time(0));
+    int r = rand() % Neighbors.size();
+    //   if (b1.y - Neighbors[r].y ==  1 && b1.x - Neighbors[r].x == 0) {
+    //     grid[b1.x][b1.y].t = true;
+    //     //grid[b1.x][b1.y - 1].b = false;
+    //   }
+    //   if (b1.x - Neighbors[r].x == -1 && b1.y - Neighbors[r].y == 0) {
+    //     grid[b1.x][b1.y].r = true;
+    //     //grid[b1.x + 1][b1.y].l = false;
+    //   }
+    //   if (b1.y - Neighbors[r].y == -1 && b1.x - Neighbors[r].x == 0) {
+    //     grid[b1.x][b1.y].b = true;
+    //     //grid[b1.x][b1.y + 1].t = false;
+    //   }
+    //   if (b1.x - Neighbors[r].x ==  1 && b1.y - Neighbors[r].y == 0) {
+    //     grid[b1.x][b1.y].l = true;
+    //     //grid[b1.x - 1][b1.y].r = false;
+    //   }
       b1.x = (Neighbors[r].x);
       b1.y = (Neighbors[r].y);
-    }
+    // }
     nb ++;
     std::cout << "N° " << (nb) << "\n I did some " << (debug) << '\n';
   }
 }
+
+//problème sur la detéction et sur l'assigement des murs...

cd43f91cf9f15bd6226b3b038f0f52209b762a7e
Agulll <61034995+Agulll@users.noreply.github.com>
2020-03-16 19:24:07 +0100

    Update test_maze.cpp

--- test_maze.cpp
+++ test_maze.cpp
@@ -18,7 +18,7 @@
     std::vector<cell> history;
   };
 
-  void show (std::array<std::array<cell, 5>, 5> grid, builder b1)
+  void show (std::array<std::array<cell, 15>, 15> grid, builder b1)
    {
     for (std::size_t i = 0; i < grid.size(); i++) {
       for (std::size_t j = 0; j < grid[i].size(); j++) {
@@ -80,7 +80,7 @@
     }
   }
 
-bool anyof (std::array<std::array<cell, 5>,5> grid){
+bool anyof (std::array<std::array<cell, 15>,15> grid){
   for (std::size_t i = 0; i < grid.size(); i++) {
     for (std::size_t j = 0; j < grid[i].size(); j++) {
       if (grid[i][j].visited == true){
@@ -95,7 +95,7 @@ bool anyof (std::array<std::array<cell, 5>,5> grid){
     builder b1;
     b1.x = 0;
     b1.y = 0;
-    std::array<std::array<cell, 5>,5> grid;
+    std::array<std::array<cell, 15>,15> grid;
     for (std::size_t i = 0; i < grid.size(); i++) {
       for (std::size_t j = 0; j < grid[i].size(); j++) {
         grid[i][j].x = i;
@@ -108,7 +108,7 @@ bool anyof (std::array<std::array<cell, 5>,5> grid){
     std::cout << "starting.\n";
     //while (!std::any_of(grid.begin(), grid.end(), [](const auto&line){return std::any_of(line.begin(),line.end(),[](const auto&cell) {return cell.visited;});}))
     //while (anyof(grid)){
-    for (int i = 0; i < 101; i++)
+    for (int i = 0; i < 1001; i++)
     {
     grid[b1.x][b1.y].visited = true;
     show(grid, b1);
@@ -117,10 +117,10 @@ bool anyof (std::array<std::array<cell, 5>,5> grid){
     if (b1.y > 0 && !grid[b1.x][b1.y - 1].visited){
       Neighbors.push_back(grid[b1.x][b1.y - 1]);
       std::cout << "push " << (grid[b1.x][b1.y - 1].x) << ',' << (grid[b1.x][b1.y - 1].y) << '\n';
-    } if (b1.x < 4 && !grid[b1.x + 1][b1.y].visited){
+    } if (b1.x < 15-1 && !grid[b1.x + 1][b1.y].visited){
       Neighbors.push_back(grid[b1.x + 1][b1.y]);
       std::cout << "push " << (grid[b1.x + 1][b1.y].x) << ',' << (grid[b1.x + 1][b1.y].y) << '\n';
-    } if (b1.y < 4 && !grid[b1.x][b1.y + 1].visited){
+    } if (b1.y < 15-1 && !grid[b1.x][b1.y + 1].visited){
       Neighbors.push_back(grid[b1.x][b1.y + 1]);
       std::cout << "push " << (grid[b1.x][b1.y + 1].x) << ',' << (grid[b1.x][b1.y + 1].y) << '\n';
     } if (b1.x < 0 && !grid[b1.x - 1][b1.y].visited){
@@ -135,30 +135,29 @@ bool anyof (std::array<std::array<cell, 5>,5> grid){
       debug = "Foward_traking";
     }
     std::srand(time(0));
-    if (Neighbors.size() != 0):
-      int r = rand() % Neighbors.size();
-        if (b1.y - Neighbors[r].y ==  1 && b1.x - Neighbors[r].x == 0) {
-          grid[b1.x][b1.y].l = true;
-          grid[b1.x][b1.y - 1].r = true;
-        }
-        if (b1.x - Neighbors[r].x == -1 && b1.y - Neighbors[r].y == 0) {
-          grid[b1.x][b1.y].b = true;
-          grid[b1.x + 1][b1.y].t = true;
-        }
-        if (b1.y - Neighbors[r].y == -1 && b1.x - Neighbors[r].x == 0) {
-          grid[b1.x][b1.y].r = true;
-          grid[b1.x][b1.y + 1].l = true;
-        }
-        if (b1.x - Neighbors[r].x ==  1 && b1.y - Neighbors[r].y == 0) {
-          grid[b1.x][b1.y].t = true;
-          grid[b1.x - 1][b1.y].b = true;
-        }
-        b1.x = (Neighbors[r].x);
-        b1.y = (Neighbors[r].y);
-        b1.history.push_back(grid[b1.x][b1.y]);
+    if (!Neighbors.empty())
+    {int r = rand() % Neighbors.size();
+      if (b1.y - Neighbors[r].y ==  1 && b1.x - Neighbors[r].x == 0) {
+        grid[b1.x][b1.y].l = true;
+        grid[b1.x][b1.y - 1].r = true;
+      } if (b1.x - Neighbors[r].x == -1 && b1.y - Neighbors[r].y == 0) {
+        grid[b1.x][b1.y].b = true;
+        grid[b1.x + 1][b1.y].t = true;
+      } if (b1.y - Neighbors[r].y == -1 && b1.x - Neighbors[r].x == 0) {
+        grid[b1.x][b1.y].r = true;
+        grid[b1.x][b1.y + 1].l = true;
+      } if (b1.x - Neighbors[r].x ==  1 && b1.y - Neighbors[r].y == 0) {
+        grid[b1.x][b1.y].t = true;
+        grid[b1.x - 1][b1.y].b = true;
       }
-      nb ++;
-      std::cout << "N° " << (nb) << "\n I did some " << (debug) << '\n';
+      b1.x = (Neighbors[r].x);
+      b1.y = (Neighbors[r].y);
+      b1.history.push_back(grid[b1.x][b1.y]);
+    }
+    nb ++;
+    std::cout << "N° " << (nb) << "\n I did some " << (debug) << '\n';
   }
+}
 
-//problème sur la detéction et sur l'assigement des murs...
+
+//problème sur le random
